72be117208b7255291ee7e2a8fcde18d1533bdc5
nshen <nshen121@gmail.com>
2021-09-16 18:52:00 +0800

    feat: add isObject / isValidObject

--- src/index.ts
+++ src/index.ts
@@ -43,8 +43,15 @@ export function isInstance<T>(value: unknown, C: ClassType<T>): value is T {
 /**
  *  This will not check element types.
  */
-export function isArray<T>(value: unknown): value is Array<T> {
+export function isArray<ItemType>(value: unknown): value is Array<ItemType> {
   return Array.isArray(value);
+}
+
+// https://www.npmjs.com/package/isobject
+export function isObject(value: unknown): value is { [key: string]: unknown } {
+  return (
+    value != null && typeof value === 'object' && Array.isArray(value) === false
+  );
 }
 
 export function has<T>(...args: Array<keyof T>) {
@@ -53,8 +60,27 @@ export function has<T>(...args: Array<keyof T>) {
   };
 }
 
+// Discriminated unions
+// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#discriminated-unions
 export function kind<T>(uid: string, uniProp: string = 'kind') {
   return function (value: unknown): value is T {
     return isString((value as any)[uniProp]) && (value as any)[uniProp] === uid;
   };
+}
+
+type Validator<T> = (value: unknown) => value is T;
+type Schema<T = {}> = { [K in keyof T]: Validator<T[K]> };
+
+export function isValidObject<T>(
+  value: unknown,
+  schema: Schema<T>
+): value is T {
+  if (isObject(value)) {
+    for (let k in schema) {
+      if (!value[k]) return false;
+      if (!schema[k](value[k])) return false;
+    }
+    return true;
+  }
+  return false;
 }
